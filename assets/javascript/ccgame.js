7373221ef7ac099e29531e4165b75a1ce269c49d
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-11-26 12:21:36 -0500

    adds updates

--- assets/javascript/ccgame.js
+++ assets/javascript/ccgame.js
@@ -1,73 +1,96 @@
-//players current score
+
+// Crystal variables and values
+var crystal = {
+	rainbow: {
+		name: "rainbow",
+		value: 0
+	}
+
+	amethyst {
+		name: "amethyst",
+		value: 0
+	}
+
+	peridot {
+		name: "peridot",
+		value: 0
+	}
+
+	diamond {
+		name: "diamond",
+		value: 0
+	}
+};
+
+
+//Players current and target scores
 var wins = 0;
 var losses = 0;
-var addition = 0;
 
-//selects a random number from 19 t0 120
-var total = Math.floor((Math.random()* 100) + 19);
+// Wins and Losses
+var winCount = 0;
+var lossCount = 0;
 
-//assigns random value to crystals from 1-12
+// calls button by id
+var button = document.getElementById("rainbow");
+var button = document.getElementById("amethyst");
+var button = document.getElementById("peridot");
+var button = document.getElementById("diamond");
+
+
+//Assigns random value to crystals from 1-12
 var rainbow = Math.floor((Math.random()* 12) + 1);
 var amethyst = Math.floor((Math.random()* 12) + 1);
 var peridot = Math.floor((Math.random()* 12) + 1);
 var diamond = Math.floor((Math.random()* 12) + 1);
 
-function myButton() {
-var button = document.getElementById("crystal");
-
-
-// //updates addition in html
-// var updateAddition = function (){
-// $('.addition').empty();
-// $('.addition').append(addition);
-// $('#wins').empty();
-// $('#wins').append(wins);
-// $('#losses').empty();
-// $('#losses').append(losses);
-// }
-
-// //restart function
-// var restart = function(){
-// addition = 0;
-// total = Math.floor((Math.random()* 100) + 19);
-
-// // $('.total').empty();
-// // $('.total').append(total);
-
-rainbow = Math.floor((Math.random()* 12) + 1);
-amethyst = Math.floor((Math.random()* 12) + 1);
-peridot = Math.floor((Math.random()* 12) + 1);
-diamond = Math.floor((Math.random()* 12) + 1);
-console.log (crystal)
-}
+//Selects a random number from 19 to 120
+var total = Math.floor((Math.random()* 100) + 19);
 
 function myButton() {
-var button = document.getElementById("demo");
-for (var i = 0; i < crystal.length; i++) {
-	crystal[i]
+
+
+// udates background image in css
+var input = $( "input:image" ).css({ background-image: "wallpaper2.jpg" }
+
+//updates addition in html
+var updateAddition = function (){
+$('.addition').empty();
+$('.addition').append(addition);
+$('#wins').empty();
+$('#wins').append(wins);
+$('#losses').empty();
+$('#losses').append(losses);
 }
 
+//restart function
+var restart = function(){
+addition = 0;
+total = Math.floor((Math.random()* 100) + 19);
+
+$('.total').empty();
+$('.total').append(total);
+
+
+alert('restart');
+updateAddition();
 }
+var logic = function (){
+if (addition == total) {
+wins = wins + 1;
 
-// //alert('restart');
-// updateAddition();
-// }
-// var logic = function (){
-// if (addition == total) {
-// wins = wins + 1;
+audio.play();
+restart();
+}else if(addition > total){
+losses = losses + 1;
 
-// audio.play();
-// restart();
-// }else if(addition > total){
-// losses = losses + 1;
-
-// audio.play();
-// restart();
-// }
-// else {
-// updateAddition();
-// }
-// }
+audio.play();
+restart();
+}
+else {
+updateAddition();
+}
+}
 
 $('.total').append(total);
 
