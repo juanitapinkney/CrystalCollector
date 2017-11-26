0f0b5697384968dadb2aef16f72ae7ce0f8bbb2f
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-11-26 10:20:49 -0500

    adds changes

--- assets/javascript/ccgame.js
+++ assets/javascript/ccgame.js
@@ -1,4 +1,4 @@
-//players currunt score
+//players current score
 var wins = 0;
 var losses = 0;
 var addition = 0;
@@ -13,57 +13,61 @@ var peridot = Math.floor((Math.random()* 12) + 1);
 var diamond = Math.floor((Math.random()* 12) + 1);
 
 function myButton() {
-var button = document.getElementById("demo");
+var button = document.getElementById("crystal");
 
 
-//updates addition in html
-var updateAddition = function (){
-$('.addition').empty();
-$('.addition').append(addition);
-$('#wins').empty();
-$('#wins').append(wins);
-$('#losses').empty();
-$('#losses').append(losses);
-}
+// //updates addition in html
+// var updateAddition = function (){
+// $('.addition').empty();
+// $('.addition').append(addition);
+// $('#wins').empty();
+// $('#wins').append(wins);
+// $('#losses').empty();
+// $('#losses').append(losses);
+// }
 
-//restart function
-var restart = function(){
-addition = 0;
-total = Math.floor((Math.random()* 100) + 19);
+// //restart function
+// var restart = function(){
+// addition = 0;
+// total = Math.floor((Math.random()* 100) + 19);
 
-$('.total').empty();
-$('.total').append(total);
+// // $('.total').empty();
+// // $('.total').append(total);
 
 rainbow = Math.floor((Math.random()* 12) + 1);
 amethyst = Math.floor((Math.random()* 12) + 1);
 peridot = Math.floor((Math.random()* 12) + 1);
 diamond = Math.floor((Math.random()* 12) + 1);
+console.log (crystal)
 }
 
 function myButton() {
 var button = document.getElementById("demo");
-
+for (var i = 0; i < crystal.length; i++) {
+	crystal[i]
 }
 
-//alert('restart');
-updateAddition();
 }
-var logic = function (){
-if (addition == total) {
-wins = wins + 1;
 
-audio.play();
-restart();
-}else if(addition > total){
-losses = losses + 1;
+// //alert('restart');
+// updateAddition();
+// }
+// var logic = function (){
+// if (addition == total) {
+// wins = wins + 1;
 
-audio.play();
-restart();
-}
-else {
-updateAddition();
-}
-}
+// audio.play();
+// restart();
+// }else if(addition > total){
+// losses = losses + 1;
+
+// audio.play();
+// restart();
+// }
+// else {
+// updateAddition();
+// }
+// }
 
 $('.total').append(total);
 
