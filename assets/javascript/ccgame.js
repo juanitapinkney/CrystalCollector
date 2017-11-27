54ef8f4fab6ecc9f73f03c510ef82dfbf2c992a9
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-11-27 18:28:25 -0500

    adds changes

--- assets/javascript/ccgame.js
+++ assets/javascript/ccgame.js
@@ -2,28 +2,35 @@
 
 $(document).ready(function(){
 
-	$('#rainbow').click(function(){
-		addition = addition + rainbow;
+	$('.rainbow').click(function(){
+		Math.random = Math.random + rainbow;
 		logic();
 	})
 
-	$('#amethyst').click(function(){
-		addition = addition + amethyst;
+	$('.amethyst').click(function(){
+		Math.random = Math.random + amethyst;
 		logic();
 	})
 
-	$('#peridot').click(function(){
-		addition = addition + crystals2;
+	$('.peridot').click(function(){
+		Math.random = Math.random + peridot;
 		logic();
 	})
 
-	$('#diamond').click(function(){
-		addition = addition + diamond;
+	$('.diamond').click(function(){
+		Math.random = Math.random + diamond;
 		logic();
 	})
 
 })
 
+
+// Assigns button by id
+
+var button = document.getElementByClass("rainbow");
+var button = document.getElementByClass("amethyst");
+var button = document.getElementByClass("peridot");
+var button = document.getElementByClass("diamond");
 
 // Assigns random value to crystals from 1-12
 
@@ -33,20 +40,10 @@ var peridot = Math.floor((Math.random()* 12) + 1);
 var diamond = Math.floor((Math.random()* 12) + 1);
 
 
-// // Selects a random number from 19 to 120
-
-// var total = Math.floor((Math.random()* 100) + 19);
-
-// function myButton() {
-// 	console.log ()
-
-// }
-
-
 // Game Function
 
 function getRandomNumber () {
-	return Math.floor((Math.random() * 12) + 1);
+	on.click = Math.floor((Math.random() * 12) + 1);
 	console.log ("Function Called")
 }
 
@@ -74,120 +71,3 @@ $("#wins").text(wins);
 $("#losses").text(losses);
 
 }
-
-
-
-
-// function storevariablecrystal()
-
-// {
-
-// var crystals = [ "rainbow", "amethyst", "peridot", "diamond"];
-
-// arrayCopier (crystals);
-
-// }
-
-// function arrayCopier() {
-
-// 	for (var i = 0; i < crystals.length; i++) {
-
-// 		newNumber.push(arrayCopier[i]);
-// 	}
-// 	console.log(arrayCopier)
-// 	console.log(newNumber)
-// }
-
-// storeVariableCrystals ();
-
-
-
-// Global variables
-
-// var mysteryNumber;
-// var rainbow;
-// var amethyst;
-// var peridot;
-// var diamond;
-
-
-// Crystal variables and values
-
-// var crystals = {
-// 	rainbow: {
-// 		name: "rainbow",
-// 		value: 0
-// 	}
-
-// 	amethyst {
-// 		name: "amethyst",
-// 		value: 0
-// 	}
-
-// 	peridot {
-// 		name: "peridot",
-// 		value: 0
-// 	}
-
-// 	diamond {
-// 		name: "diamond",
-// 		value: 0
-// 	}
-// };
-
-
-
-// // Calls button by id
-
-// var button = document.getElementById("rainbow");
-// var button = document.getElementById("amethyst");
-// var button = document.getElementById("peridot");
-// var button = document.getElementById("diamond");
-
-
-// // Updates addition in html
-
-// var updateAddition = function (){
-// $('.addition').empty();
-// $('.addition').append(addition);
-// $('#wins').empty();
-// $('#wins').append(wins);
-// $('#losses').empty();
-// $('#losses').append(losses);
-// }
-
-
-// // Restart function
-
-// var restart = function(){
-// addition = 0;
-// total = Math.floor((Math.random()* 100) + 19);
-
-// $('.total').empty();
-// $('.total').append(total);
-
-
-// alert('restart');
-// updateAddition();
-// }
-// var logic = function (){
-// if (addition == total) {
-// wins = wins + 1;
-
-// audio.play();
-// restart();
-// }else if(addition > total){
-// losses = losses + 1;
-
-// audio.play();
-// restart();
-// }
-// else {
-// updateAddition();
-// }
-// }
-
-// $('.total').append(total);
-
-// $('.addition').append(addition);
-
